18ddeb019fa67180cb4049177cc2dca291744ce6
执笔绘浅香 <37764940+qian-xiang@users.noreply.github.com>
2018-11-21 16:37:01 +0800

    Update Functions.php
    修改敏感信息

--- app/Common/Functions.php
+++ app/Common/Functions.php
@@ -4,7 +4,7 @@
  * @return boolean 合法返回真，否则返回假
  */
 function checkReferer(){
-    $refererArray = ['https://servicewechat.com/wx1c764be0c4ca413a/0/page-frame.html','https://servicewechat.com/wx1c764be0c4ca413a/devtools/page-frame.html'];
+    $refererArray = ['https://servicewechat.com/12345678/0/page-frame.html','https://servicewechat.com/12345678/devtools/page-frame.html'];
     $refererGoal = $_SERVER['HTTP_REFERER'];
     $response = in_array($refererGoal, $refererArray)? true : false;
 
